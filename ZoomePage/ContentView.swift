66d77051f6a350ade7229f8e7e5bacbd8a1b1ffb
Hupendra Raghuwanshi <hupendra.raghuwanshi@aplitetech.com>
2022-12-20 11:05:28 +0530

    Initial Commit

---
+++ ZoomePage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZoomePage
+//
+//  Created by Apple on 20/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZoomePage/ZoomePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomePageApp.swift
+//  ZoomePage
+//
+//  Created by Apple on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ZoomePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
